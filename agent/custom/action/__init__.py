ec74ed84bc084c2b7c5df78e269f1e0b1648d9e2
Syoius <syoius@gmail.com>
2025-10-29 16:32:01 +0800

    feat: 望祈丰年 (#380)
    * feat: 望祈丰年繁中
    
    * fix: 题库增补
    
    * feat: 适配小鸟白露签到
    
    * fix: 领取奖励
    
    * feat: 望祈丰年 简中
    
    * chore: 更新公告

--- agent/custom/action/__init__.py
+++ agent/custom/action/__init__.py
@@ -1,5 +1,12 @@
 from .autoanswer import *
 from .copilotinfo import *
 from .monopoly import *
+from .general_autoanswer import *
 
-__all__ = ["AutoAnswer", "CopilotInfo", "DownRestart", "MonopolySetShipDestination"]
+__all__ = [
+    "AutoAnswer",
+    "CopilotInfo",
+    "DownRestart",
+    "MonopolySetShipDestination",
+    "GeneralAutoAnswer",
+]
